0d7aa68633d86de2e00535b8027c70725fd076cb
stian røgeberg <srr007@hotmail.com>
2024-09-26 11:47:41 +0200

    The project v. 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./Header.js";
+import Main from "./Main.js";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
